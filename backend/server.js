a3b03663d46c3d40ee250f5c4102653c50f12636
kacasih <branko.jozko@gmail.com>
2023-08-27 21:04:32 +0200

    many changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const bodyparser = require('body-parser');
 
 const app = express();
 
@@ -13,6 +14,26 @@ app.get('/', (req,res) => {//home page handling get requests with response to re
 res.render('main');
 });
 
+app.get('/login', (req,res) => {
+  res.render('login');
+});
+
+
+app.get('/signup', (req,res) => {
+  res.render('signup');
+});
+
+app.get('/aboutus', (req,res) => {
+  res.render('aboutus');
+});
+
+app.get('/contactus', (req,res) => {
+  res.render('contactus');
+});
+
+app.get('/leaderboard', (req,res) => {
+  res.render('leaderboard');
+});
 app.listen(4000, () => {
 console.log('server listening on port 4000')
 });
